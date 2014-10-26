55ebd100a1c29211c0811055d2a1c6074301f270
cristiprg <cristian.prigoana@yahoo.com>
2014-10-26 21:20:46 +0200

    init

---
+++ Ass1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ass1.Startup))]
+namespace Ass1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
