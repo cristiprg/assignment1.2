9af2cbad1c8507f60dd6a41c41a84ee38ec230b4
cristiprg <cristian.prigoana@yahoo.com>
2014-10-27 13:43:39 +0200

    Print/Get time

--- Ass1/Controllers/HomeController.cs
+++ Ass1/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ass1.Models;
+using Microsoft.Ajax.Utilities;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Ass1.Controllers
 {
@@ -11,8 +16,18 @@ namespace Ass1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = TimeHelper.getTime();
-            return View();
+            if (Request.IsAuthenticated)
+            {
+                // https://github.com/rustd/AspnetIdentitySample/blob/master/AspnetIdentitySample/Controllers/HomeController.cs
+                // Instantiate the ASP.NET Identity system
+                var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                // Get the current logged in User and look up the user in ASP.NET Identity
+                var currentUser = manager.FindById(User.Identity.GetUserId()); 
+
+                ViewBag.Message = TimeHelper.getTime(currentUser.Latitude, currentUser.Longitude);                
+            }
+
+        return View();
         }
 
         public ActionResult About()
@@ -27,6 +42,6 @@ namespace Ass1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
+        }       
     }
 }
